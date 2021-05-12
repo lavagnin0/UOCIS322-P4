d7d69011219fa8f553e954c05e8311d9499d3018
Jeremy Lavagnino <jlavagni@uoregon.edu>
2021-05-11 19:21:06 -0700

    Initial updates

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -26,14 +26,23 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    if control_dist_km < 200:
+        max_speed = 34
+    elif control_dist_km < 400:
+        max_speed = 32
+    elif control_dist_km < 600:
+        max_speed = 30
+    else:
+        max_speed = 28
+    return brevet_start_time.shift(hours=control_dist_km/max_speed)
+
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
     Args:
        control_dist_km:  number, control distance in kilometers
-          brevet_dist_km: number, nominal distance of the brevet
+        brevet_dist_km: number, nominal distance of the brevet
           in kilometers, which must be one of 200, 300, 400, 600, or 1000
           (the only official ACP brevet distances)
        brevet_start_time:  A date object (arrow)
@@ -41,4 +50,8 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    if control_dist_km > 600:
+        min_speed = 11.428
+    else:
+        min_speed = 15
+    return brevet_start_time.shift(hours=control_dist_km/min_speed)
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -52,14 +52,16 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    start_time = arrow.get(request.args.get('start_time', type=str), 'YYYY-MM-DDTHH:mm')
     app.logger.debug("km={}".format(km))
+    app.logger.debug("start time={}".format(start_time))
     app.logger.debug("request.args: {}".format(request.args))
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    open_time = acp_times.open_time(km, 200, start_time).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, 200, start_time).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
