ebe43cf4ae948ef4d79852657b3c1a9727f6a5ee
Jeremy Lavagnino <jlavagni@uoregon.edu>
2021-05-13 14:09:18 -0700

    Update acp_times.py

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -26,16 +26,15 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    if control_dist_km < 200:
+    if control_dist_km <= 200:
         max_speed = 34
-    elif control_dist_km < 400:
+    elif control_dist_km <= 400:
         max_speed = 32
-    elif control_dist_km < 600:
+    elif control_dist_km <= 600:
         max_speed = 30
     else:
         max_speed = 28
     return brevet_start_time.shift(hours=control_dist_km/max_speed)
-
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -50,8 +49,8 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    if control_dist_km > 600:
-        min_speed = 11.428
-    else:
+    if control_dist_km <= 600:
         min_speed = 15
+    else:
+        min_speed = 11.428
     return brevet_start_time.shift(hours=control_dist_km/min_speed)
